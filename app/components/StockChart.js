93999d007b8dd67c8cbd3612f533725e2b867af7
Tung Vo <tung.tan.vo@gmail.com>
2018-09-30 08:51:55 -0700

    update style some more

--- app/components/StockChart.js
+++ app/components/StockChart.js
@@ -4,18 +4,20 @@ import styles from '../style.css';
 class StockRow extends React.Component {
     render() {
         return (
-            <div>
-                <div className="column-sm-4">
-                    <div>
-                        {this.props.stockList.map( (stock) => {
-                                return (
-                                    <StockChart name={stock.name} url={stock.url} /> 
-                                )
-                            }) 
-                        }
+            <section>
+                <div className="container-fluid">
+                    <div className="container">
+                        <div className="row">
+                            {this.props.stockList.map( (stock) => {
+                                    return (
+                                        <StockChart name={stock.name} url={stock.url} /> 
+                                    )
+                                }) 
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         )
     }
 }
@@ -23,12 +25,14 @@ class StockRow extends React.Component {
 class StockChart extends React.Component {
     render() {
         return (
-            <div className={styles.card}>
-                <div className={styles.title}>
-                    <i className={styles.fa} aria-hidden="true"></i>
-                    <h2>{this.props.name}</h2>
+            <div className="col-sm-4">
+                <div className={styles.card}>
+                    <div className={styles.title}>
+                        <i className={styles.fa} aria-hidden="true"></i>
+                        <h2>{this.props.name}</h2>
+                    </div>
+                    <iframe src={this.props.url} frameBorder="0" height="385" width="380"></iframe>
                 </div>
-                <iframe src={this.props.url} frameBorder="0" height="385" width="380"></iframe>
             </div>
         )
     }
