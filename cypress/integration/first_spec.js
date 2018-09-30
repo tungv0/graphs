817361ae4d9eae088debe7917cbad56b85e2b1ae
Tung Vo <tung.tan.vo@gmail.com>
2018-09-30 09:24:09 -0700

    add cypress tests

---
+++ cypress/integration/first_spec.js
@@ -0,0 +1,12 @@
+describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(true)
+    })
+  })
+
+
+  describe('My First Graph Test', function() {
+    it('Visits Stock Graphs Page', function() {
+      cy.visit('http://localhost:8080')
+    })
+  })
