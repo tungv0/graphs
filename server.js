7af35ba77538a1e84441d62fccd9be49395e2735
Tung Vo <tung.tan.vo@gmail.com>
2019-01-24 09:38:41 -0800

    Add a few new routes for an excercise

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -5,9 +6,48 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/build/index.html');
 });
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
+
+// Respond not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }
+})
 
 app.listen(PORT, error => {
   error
