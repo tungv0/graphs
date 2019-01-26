ae78ddab991d73732e32b94ec5b0edd9827965aa
Tung Vo <tung.tan.vo@gmail.com>
2019-01-25 17:34:57 -0800

    add name path and supports query string

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-
+var name = '';
 
 // --> 7)  Mount the Logger middleware here
 app.use(
@@ -70,7 +70,15 @@ app.get('/:word/echo',
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get(
+    function(request, response) {
+        console.log(`${request.method} ${request.path} - ${name}`);
+        response.send({name: name});
+}).post(
+    function(request, response){
+        console.log(`${request.method} ${request.path} - ${request.query}`);
+        name = request.query.first + ' ' + request.query.last;        
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
