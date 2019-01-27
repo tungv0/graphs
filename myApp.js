7e6afba992fb520127aea3da021568563e565b68
Tung Vo <tung.tan.vo@gmail.com>
2019-01-27 14:07:56 -0800

    add url shortener endpoint

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 var name = '';
+var shortenedURL = {};
 
 // --> 7)  Mount the Logger middleware here
 app.use(
@@ -135,25 +136,30 @@ app.get('/api/whoami',
     }
 );
 
-// {
-//     "host": "ancient-coast-93792.herokuapp.com",
-//     "connection": "close",
-//     "cache-control": "max-age=0",
-//     "upgrade-insecure-requests": "1",
-//     "user-agent": "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/71.0.3578.98 Safari\/537.36",
-//     "accept": "text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/webp,image\/apng,*\/*;q=0.8",
-//     "accept-encoding": "gzip, deflate, br",
-//     "accept-language": "en-US,en;q=0.9",
-//     "if-none-match": "W\/\"18-nUKBwp4q2yy3tqbcU8ShNA6o+UQ\"",
-//     "x-request-id": "2c048b45-bace-4fc5-9a8a-d9108fb792e1",
-//     "x-forwarded-for": "67.188.3.125",
-//     "x-forwarded-proto": "https",
-//     "x-forwarded-port": "443",
-//     "via": "1.1 vegur",
-//     "connect-time": "1",
-//     "x-request-start": "1548544691144",
-//     "total-route-time": "0"
-//   }
+/** Project 3) URL shortener */
+app.post('/api/shorturl/new',
+    function(request, response) {
+        console.log(request.body);
+        let url = request.body.original_url;
+        let key = Object.keys(shortenedURL).length;
+
+        shortenedURL[key] = url;
+        response.send({
+            original_url: url,
+            short_url: key
+        })
+
+        console.log(`current list of shortened URLs: ${JSON.stringify(shortenedURL)}`);
+    }
+);
+
+app.get('/api/shorturl/:id', 
+    function(request, reponse) {
+        let url = shortenedURL[request.params.id];
+
+        // TODO: redirect to url
+    }
+);
 
 
 // This would be part of the basic setup of an Express app
