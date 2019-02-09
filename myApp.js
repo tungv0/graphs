e68f0eb794c08fc95a710ced8169a4f514e3478f
Tung Vo <tung.tan.vo@gmail.com>
2019-02-09 13:14:35 -0800

    add a bunch of infosec helmet funcdtions

--- myApp.js
+++ myApp.js
@@ -712,7 +712,7 @@ app.use(helmet.noCache());
 // in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
 // so it needs to be enclosed in **double quotes** to be working.
 
-app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self"], scriptSrc: ['trusted-cdn.com']}}));
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));
 
 /** TIP: */ 
 
