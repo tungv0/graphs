4ead6d630ed9f01c59edb89734b21bae11868829
Tung Vo <tung.tan.vo@gmail.com>
2019-02-10 09:06:44 -0800

    add bcrypt support

--- server.js
+++ server.js
@@ -62,6 +62,29 @@ app.get('/server.js', function(req, res, next) {
   });
 });
 
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+  });
+});
+
+
+//END_ASYNC
+
+//START_SYNC
+
+
+
+//END_SYNC
+
 app.use(function(req, res, next){
   res.status(404).type('txt').send('Not Found');
 });
