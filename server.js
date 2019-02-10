f29758f6511292cec5d9b639bc5b50204b5347ac
Tung Vo <tung.tan.vo@gmail.com>
2019-02-10 09:11:15 -0800

    add bcrypt support

--- server.js
+++ server.js
@@ -70,10 +70,10 @@ const someOtherPlaintextPassword = 'pass123';
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); //true
-  });
+  // console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  // bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  //     console.log(res); //true
+  // });
 });
 
 
