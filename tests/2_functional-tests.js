c4c9c28bf50c10e19024dc176b1cea71850333d6
Tung Vo <tung.tan.vo@gmail.com>
2019-02-11 09:43:28 -0800

    add mocha/chai testing support

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -252,7 +252,7 @@ suite('Functional Tests', function() {
         // assert that the element(s) 'span#dates' exist and their count is 1
         browser
           .fill('surname', 'Colombo')
-          .pressButton('submit', function(){
+          .pressButton('submit', function(done){
             
             /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
             
@@ -280,8 +280,25 @@ suite('Functional Tests', function() {
         // assert that the text inside the element 'span#name' is 'Amerigo'
         // assert that the text inside the element 'span#surname' is 'Vespucci'
         // assert that the element(s) 'span#dates' exist and their count is 1
-        assert.fail();
-        done();
+        browser
+          .fill('surname', 'Vespucci')
+          .pressButton('submit', function(){
+            
+            /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
+            
+            // pressButton is Async.  Waits for the ajax call to complete...
+
+            // assert that status is OK 200
+            browser.assert.success();
+            // assert that the text inside the element 'span#name' is 'Cristoforo'
+            browser.assert.text('span#name', 'Amerigo');
+            // assert that the text inside the element 'span#surname' is 'Colombo'
+            browser.assert.text('span#surname', 'Vespucci');
+            // assert that the element(s) 'span#dates' exist and their count is 1
+            browser.assert.element('span#dates', 1);
+            
+            done();   // It's an async test, so we have to call 'done()''
+          });
       
       });
     });
