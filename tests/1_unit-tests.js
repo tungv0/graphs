c5dc09b963c72642e5720ecd6355a44e5f650208
Tung Vo <tung.tan.vo@gmail.com>
2019-02-10 17:30:45 -0800

    add bcrypt support

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -103,7 +103,7 @@ suite('Unit Tests', function(){
     // it should be less than 1
     test('#approximately', function() {
       assert.approximately(weirdNumbers(0.5) , 1, 0.5 );
-      assert.approximately(weirdNumbers(0.2) , 1, 0.2 );
+      assert.approximately(weirdNumbers(0.2) , 1, 0.8 );
     });
   });
 
