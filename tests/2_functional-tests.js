b0317055984aeb2a9a711ec26013a57cc6a8b22b
Tung Vo <tung.tan.vo@gmail.com>
2019-02-14 10:42:31 -0800

    add mocha/chai testing support

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -221,7 +221,7 @@ suite('Functional Tests', function() {
       test('#example - submit the input "surname" : "Polo"', function(done) {
         browser
           .fill('surname', 'Polo')
-          .pressButton('submit', function(done) {
+          .pressButton('submit', function() {
             // pressButton is ## Async ##.  
             // It waits for the ajax call to complete...
 
@@ -252,20 +252,20 @@ suite('Functional Tests', function() {
         // assert that the element(s) 'span#dates' exist and their count is 1
         browser
           .fill('surname', 'Colombo')
-          .pressButton('submit', function(done) {
+          .pressButton('submit', function() {
             
             /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
             
             // pressButton is Async.  Waits for the ajax call to complete...
 
-            // // assert that status is OK 200
-            // browser.assert.success();
-            // // assert that the text inside the element 'span#name' is 'Cristoforo'
-            // browser.assert.text('span#name', 'Cristoforo');
-            // // assert that the text inside the element 'span#surname' is 'Colombo'
-            // browser.assert.text('span#surname', 'Colombo');
-            // // assert that the element(s) 'span#dates' exist and their count is 1
-            // browser.assert.element('span#dates', 1);
+            // assert that status is OK 200
+            browser.assert.success();
+            // assert that the text inside the element 'span#name' is 'Cristoforo'
+            browser.assert.text('span#name', 'Cristoforo');
+            // assert that the text inside the element 'span#surname' is 'Colombo'
+            browser.assert.text('span#surname', 'Colombo');
+            // assert that the element(s) 'span#dates' exist and their count is 1
+            browser.assert.element('span#dates', 1);
             
             done();   // It's an async test, so we have to call 'done()''
           });
@@ -282,7 +282,7 @@ suite('Functional Tests', function() {
         // assert that the element(s) 'span#dates' exist and their count is 1
         browser
           .fill('surname', 'Vespucci')
-          .pressButton('submit', function(done) {
+          .pressButton('submit', function() {
             
             /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
             
