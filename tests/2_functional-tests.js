667fcff1c6f39e8bf13a2032f64af94b97afce99
Tung Vo <tung.tan.vo@gmail.com>
2019-02-14 11:03:27 -0800

    add mocha/chai testing support

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -250,8 +250,8 @@ suite('Functional Tests', function() {
         // assert that the text inside the element 'span#name' is 'Cristoforo'
         // assert that the text inside the element 'span#surname' is 'Colombo'
         // assert that the element(s) 'span#dates' exist and their count is 1
-        browser.fill('surname', 'Colombo')
-        browser.pressButton('submit', function() {
+        browser.fill('surname', 'Colombo');
+        browser.pressButton('submit', function(done) {
             
             /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
             
